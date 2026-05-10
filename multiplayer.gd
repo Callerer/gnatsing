b69b12845c12c907b8d5b2dfc71548ff682200b4
Callerer <callererserror@proton.me>
2026-05-10 22:33:05 +0530

    initial commit

--- multiplayer.gd
+++ multiplayer.gd
@@ -1,13 +1,27 @@
 extends Node
 
+var player_name = ""
+@export var player_scene: PackedScene
+
 func _ready() -> void:
 	multiplayer.peer_connected.connect(peer_connect_callback)
-
-func _process(_delta: float) -> void:
-	pass
 	
+# when peer joins
 func  peer_connect_callback(id):
 	$UI/VBoxContainer/InGameConsole.console("Player with id %d has joined."%[id])
+
+#console log for ALL instances [awesome concept of rpc btw!]
+@rpc("any_peer","call_local")
+func rpc_console_log(text):
+	$UI/VBoxContainer/InGameConsole.console(text)
+	
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ENTER:
+			chat()
+			
+		
 	
 func _on_host_pressed() -> void:
 	# create a multiplayer instance
@@ -23,6 +37,8 @@ func _on_host_pressed() -> void:
 	
 	multiplayer.multiplayer_peer = peer
 	print("Starting game as a server")
+	_add_player()
+	start_game()
 
 
 func _on_connect_pressed() -> void:
@@ -39,4 +55,19 @@ func _on_connect_pressed() -> void:
 	
 	multiplayer.multiplayer_peer = peer
 	print("Starting game as a client")
+	_add_player()
+	start_game()
+
+func  _add_player():
+	var player = player_scene.instantiate()
+	call_deferred("add_child",player)
 	
+func start_game():
+	player_name = $UI/VBoxContainer/NetworkOprions/Name.text
+	
+func chat():
+	var chatbox = $UI/VBoxContainer/ChatBox
+	var chatbox_string = chatbox.text
+	chatbox.clear()
+	chatbox_string = player_name + ": " + chatbox_string
+	rpc_console_log.rpc(chatbox_string)
